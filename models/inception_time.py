15d92a7abc3431a031a52666f9db396b858bc0a1
ngcaonghi <nghi.nguyen.180028@student.fulbright.edu.vn>
2022-02-06 18:11:42 +0700

    Update InceptionTime

--- models/inception_time.py
+++ models/inception_time.py
@@ -13,12 +13,11 @@ class InceptionTime(nn.Module):
         - incept_kernels (tuple(int)): a 3-tuple of integers representing the 3
         kernel sizes of conv1d layers within each inception module.
         - avg_pool_kernel (int): the kernel size of the average pooling layer
-        - fc1_input_size (int): the size of the first fully connected layer
-        - num_eeg_channels (int): number of EEG channels
         - output_size (int): the length of the output, i.e. the dimensionality
         of the embedding space
-        - conv_0_1, conv_0_2, conv_1, conv_2, conv_3, res, ap1d, fc1, fc2 
-        (nn.Module): torch neural net modules.
+        - conv_1, conv_2, conv_3, res, ap1d, fc1, fc2 (nn.Module): torch neural 
+        net modules.
+        - 
     
     Methods:
         - __init__(incept_depth, incept_kernels, avg_pool_kernel, fc1_input_size, 
@@ -27,42 +26,67 @@ class InceptionTime(nn.Module):
         - _shortcut
         - forward(s)
     '''
-    def __init__(self, incept_depth=9, incept_kernels=(33, 17, 9), 
-                avg_pool_kernel=64, fc1_input_size=280, num_eeg_channels=14,
-                output_size=14):
+    def __init__(self, incept_depth=12, incept_kernels=(129, 65, 33, 17), 
+                avg_pool_kernel=64, fc=True, output_size=50):
         '''
         Init method.
         Paramaters:
             - incept_depth (int): number of inception modules. Default at 9.
-            - incept_kernels (tuple(int)): a 3-tuple of integers representing 
-            the 3 kernel sizes of conv1d layers within each inception module.
-            Should be odd numbers. Default at (33, 17, 9). 
+            - incept_kernels (tuple(int)): a 4-tuple of integers representing 
+            the 4 kernel sizes of conv1d layers within each inception module.
+            Should be odd numbers sampled from a logarithmic sequence and be in
+            a decreasing order. Default at (129, 65, 33, 17). 
             - avg_pool_kernel (int): the kernel size of the average pooling 
             layer. Default at 64.
-            - fc1_input_size (int): the size of the first fully connected layer.
-            Default at 280.
-            - num_eeg_channels (int): number of EEG channels. Default at 14.
+            - fc (bool): whether the model includes fully connected layers at
+            the end. Default at True.
             - output_size (int): the length of the output, i.e. the 
-            dimensionality of the embedding space. Default at 14.
+            dimensionality of the embedding space. Default at 50.
         '''
         super(InceptionTime, self).__init__()
-        self.incept_depth = incept_depth
-        self.incept_kernels = incept_k
-        self.avg_pool_kernel = avg_pool_kernel
-        self.fc1_input_size = fc1_input_size
-        self.num_eeg_channels = num_eeg_channels
-        self.output_size = output_size
-        k_1, k_2, k_3 = incept_k
-        num_ch_1 = num_eeg_channels // 3
-        num_ch_2 = (num_eeg_channels - num_ch_1) // 2
-        num_ch_3 = num_eeg_channels - num_ch_2 - num_ch_1
 
-        # inception layers
-        self.conv_0_1 = nn.Conv1d(self.num_eeg_channels, 4, kernel_size=1)
-        self.conv_0_2 = nn.Conv1d(self.num_eeg_channels, 5, kernel_size=1)
-        self.conv_1 = nn.Conv1d(num_ch_1, num_ch_1, kernel_size=k_1, padding=k_1//2)
-        self.conv_2 = nn.Conv1d(num_ch_2, num_ch_2, kernel_size=k_2, padding=k_2//2)
-        self.conv_3 = nn.Conv1d(num_ch_3, num_ch_3, kernel_size=k_3, padding=k_3//2)
+        # default attributes or values specific to the EEG data
+        self._NUM_CHANNELS = 14
+        self._SFREQ = 256
+        self._EPOCH_LEN = 5
+        self._DELTA_NUM_FILTERS = 4
+        self._THETA_NUM_FILTERS = 3
+        self._ALPHA_NUM_FILTERS = 4
+        self._BETA_NUM_FILTERS = 3
+
+        # atrributes from init arguments
+        self.incept_depth = incept_depth
+        self.incept_kernels = incept_kernels
+        self.avg_pool_kernel = avg_pool_kernel
+        self.fc = fc
+        self.output_size = output_size
+        k_1, k_2, k_3, k_4 = incept_kernels
+
+        # convolutional layers for inception modules
+        self.conv_1 = nn.Sequential(
+            nn.Conv1d(self.num_eeg_channels, self._DELTA_NUM_FILTERS, 
+                      kernel_size=1),
+            nn.Conv1d(self._DELTA_NUM_FILTERS, self._DELTA_NUM_FILTERS, 
+                      kernel_size=k_1, padding=k_1//2)
+        )
+        self.conv_2 = nn.Sequential(
+            nn.Conv1d(self.num_eeg_channels, self._THETA_NUM_FILTERS, 
+                      kernel_size=1),
+            nn.Conv1d(self._THETA_NUM_FILTERS, self._THETA_NUM_FILTERS,
+                      kernel_size=k_2, padding=k_2//2)
+        )      
+        self.conv_3 = nn.Sequential(
+            nn.Conv1d(self.num_eeg_channels, self._ALPHA_NUM_FILTERS, 
+                      kernel_size=1),
+            nn.Conv1d(self._ALPHA_NUM_FILTERS, self._ALPHA_NUM_FILTERS, 
+                                kernel_size=k_3, padding=k_3//2)
+        )
+        self.conv_4 = nn.Sequential(
+            nn.Conv1d(self.num_eeg_channels, self._BETA_NUM_FILTERS, 
+                      kernel_size=1),
+            nn.Conv1d(self._BETA_NUM_FILTERS, self._BETA_NUM_FILTERS, 
+                                kernel_size=k_4, padding=k_4//2)
+        )
 
         # res layers
         self.res = nn.Conv1d(num_eeg_channels, num_eeg_channels, kernel_size=1)
@@ -71,6 +95,8 @@ class InceptionTime(nn.Module):
         self.ap1d = nn.AvgPool1d(avg_pool_kernel)
 
         # fully connected
+        fc1_input_size = (self._SFREQ * self._EPOCH_LEN * self._NUM_CHANNELS) \
+                        // avg_pool_kernel
         self.fc1 = nn.Linear(fc1_input_size, output_size * 5)
         self.fc2 = nn.Linear(output_size * 5, output_size)
 
@@ -79,12 +105,11 @@ class InceptionTime(nn.Module):
         '''
         Define an inception module.
         '''
-        input_1 = self.conv_0_1(input)
-        input_2 = self.conv_0_2(input)
         conv_layers = []
-        conv_layers.append(self.conv_1(input_1))
-        conv_layers.append(self.conv_2(input_2))
-        conv_layers.append(self.conv_3(input_2))
+        conv_layers.append(self.conv_1(input))
+        conv_layers.append(self.conv_2(input))
+        conv_layers.append(self.conv_3(input))
+        conv_layers.append(self.conv_4(input))
         x = torch.cat(conv_layers, 1)
         x = F.relu(x)
         return x
@@ -119,7 +144,8 @@ class InceptionTime(nn.Module):
                 input_res = x
         x = self.ap1d(x)
         x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = self.fc2(x)
+        if self.fc:
+            x = self.fc1(x)
+            x = self.fc2(x)
         x = x.view(x.size()[0], -1)
         return x
